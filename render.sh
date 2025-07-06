83f02eef9c60e9130f4322be2ced5a067e8fae52
Tom Palmer <remlapmot@hotmail.com>
2025-07-06 06:24:22 +0100

    Create render.sh

---
+++ render.sh
@@ -0,0 +1,11 @@
+quarto render --profile r-questions -o tutorial-r-questions.html
+quarto render --profile r-solutions -o tutorial-r-solutions.html
+
+quarto render --profile python-questions -o tutorial-python-questions.html
+quarto render --profile python-solutions -o tutorial-python-solutions.html
+
+quarto render --profile stata-questions -o tutorial-stata-questions.html
+quarto render --profile stata-solutions -o tutorial-stata-solutions.html
+
+quarto render --profile julia-questions -o tutorial-julia-questions.html
+quarto render --profile julia-solutions -o tutorial-julia-solutions.html
