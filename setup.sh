30261372e042bff59733f43f71716824d5106956
Tom Palmer <remlapmot@hotmail.com>
2025-07-06 06:28:52 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# A record of the commands I used to create the .venv
+uv init --python 3.13.5
+uv add uv add jupyterlab nbstata jupyterlab_stata_highlight2 papermill
+
+# To restore the .venv
+uv sync
+# Then activate the virtual environment, etc
+source .venv/bin/activate
+# Windows: source .venv/Scripts/activate
